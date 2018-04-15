028eff69429847767a727f48c98e791471232533
Biser Atanasov <biser.p.atanasov@gmail.com>
2018-04-15 20:11:30 +0300

    Some improvements to make it work smoother

--- Arduino_Remote_Lights/Arduino_Remote_Lights.ino
+++ Arduino_Remote_Lights/Arduino_Remote_Lights.ino
@@ -4,12 +4,12 @@ const int LIGHTS_PIN = 6;
 const int POT_PIN = A0;
 const int RECV_PIN = 11;
 
-const long long MAX_IR_VAL = 3772834333;
-const long long MIN_IR_VAL = 3772797613;
-const long long DEC_IR_VAL_1 = 3772781293;
-const long long DEC_IR_VAL_2 = 3772805773;
-const long long INC_IR_VAL_1 = 3772818013;
-const long long INC_IR_VAL_2 = 3772779253;
+#define MAX_IR_VAL 3772834333
+#define MIN_IR_VAL 3772797613
+#define DEC_IR_VAL_1 3772781293
+#define DEC_IR_VAL_2 3772805773
+#define INC_IR_VAL_1 3772818013
+#define INC_IR_VAL_2 3772779253
 
 int brightness = 0;
 int potVal = 0;
@@ -20,13 +20,21 @@ decode_results results;
 
 void handleIR() {
   if (irrecv.decode(&results)) {
-    
+
     if (results.value == MAX_IR_VAL) {
-      brightness = 255;
+      while (brightness < 255) {
+        brightness++;
+        setBrightness(brightness);
+        delay(8);
+      }
     }
 
     if (results.value == MIN_IR_VAL) {
-      brightness = 0;
+      while (brightness > 0) {
+        brightness--;
+        setBrightness(brightness);
+        delay(8);
+      }
     }
 
     if (results.value == DEC_IR_VAL_1 || results.value == DEC_IR_VAL_2 ) {
@@ -36,7 +44,7 @@ void handleIR() {
     if (results.value == INC_IR_VAL_1 || results.value == INC_IR_VAL_2 ) {
       brightness = constrain(brightness - 5, 0, 255);
     }
-    
+
     irrecv.resume();
   }
 }
@@ -45,7 +53,7 @@ void handlePot() {
   int currentPotVal = getPotVal();
   // First conditions takes care of potentiometer fluctuations
   // Second condition checks if the pot is touched so it doesnt assing a value if the system is just turned on
-  if ((currentPotVal < potVal - 3 || currentPotVal > potVal + 3) && initialPotVal != currentPotVal) { 
+  if ((currentPotVal < potVal - 3 || currentPotVal > potVal + 3) && initialPotVal != currentPotVal) {
     brightness = currentPotVal;
     if (brightness < 5) {
       brightness = 0;
@@ -61,6 +69,10 @@ int getPotVal() {
   return map(analogRead(POT_PIN), 0, 1023, 0, 255);
 }
 
+void setBrightness(int brightnessToSet) {
+  analogWrite(LIGHTS_PIN, brightnessToSet);
+}
+
 void setup()
 {
   irrecv.enableIRIn();
@@ -73,6 +85,6 @@ void setup()
 void loop() {
   handleIR();
   handlePot();
-  analogWrite(LIGHTS_PIN, brightness);
+  setBrightness(brightness);
 }
 
